d825b2d62983bebe2ffc607ef92b648ccb7fe420
RafaelGoulartB <rafaelgoulartb@gmail.com>
2019-02-24 11:51:22 -0300

    Hello World

---
+++ main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', () => {
+  console.log('Hello World');
+  let mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+});
+
+app.on('window-all-closed', () => app.quit())
